fa71a17bab08e3f204f9aa929dc9a20d30f60495
amancio torres <amancio.torres@uacj.com>
2026-02-16 11:53:22 -0700

    [Modifica] se modifico el proyecto de fromularios dinamicos y se elminiaron carpetas duplicadas

--- Parcial 1/Actividades_clase/Formularios_dinamicos/js/script.js
+++ Parcial 1/Actividades_clase/Formularios_dinamicos/js/script.js
@@ -20,9 +20,28 @@ boton_guardar.addEventListener("click",(e)=>{
 
 })
 
+function cambiarNumero(event){
+    const numeroElementos = event.target.value;
+    const contenido = document.querySelector("#contenedor_correos");
+    contenido.innerHTML = " "; //limpia la etiqueta y su contenido antes de agregar
+    for(let i = 1; i <= numeroElementos ; i++ ){
+        //se agrega contenido usando la insercion de html por medio del 
+        //innerHTML, que agregara todo lo que esta dentro de htmlAgregar
+        //este metodo reemplaza todo o que esta dentro de la etiqueta por 
+        //lo nuevo que se quiere agregar
+        
+        const htmlAgregar = `<label for="correo-${i}">Ingrese el correo ${i}</label>
+            <input type="email" name="correo-${i}" id="correo-${i}">
+            <br>`;
+
+        contenido.innerHTML += htmlAgregar;
+    }
+}
+
 class Usuario{
     constructor(nom, ape){
         this.nombre = nom; 
         this.apellido = ape;
     }
 }
+ 
